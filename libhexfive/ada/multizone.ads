14a3a5eb36d6a38fb6b18e6f7cb6fd4856d7da8f
Boran Car <boran.car@gmail.com>
2019-04-25 23:36:32 +0100

    Fix int -> Unsigned_64 in libhexfive

--- libhexfive/ada/multizone.ads
+++ libhexfive/ada/multizone.ads
@@ -35,37 +35,37 @@ package MultiZone is
    procedure ECALL_CSRC_MIE;  -- libhexfive.h:18
    pragma Import (C, ECALL_CSRC_MIE, "ECALL_CSRC_MIE");
 
-   procedure ECALL_CSRW_MTIMECMP;  -- libhexfive.h:20
+   procedure ECALL_CSRW_MTIMECMP (arg1 : Unsigned_64);  -- libhexfive.h:20
    pragma Import (C, ECALL_CSRW_MTIMECMP, "ECALL_CSRW_MTIMECMP");
 
-   function ECALL_CSRR_MTIME return int;  -- libhexfive.h:22
+   function ECALL_CSRR_MTIME return Unsigned_64;  -- libhexfive.h:22
    pragma Import (C, ECALL_CSRR_MTIME, "ECALL_CSRR_MTIME");
 
-   function ECALL_CSRR_MCYCLE return int;  -- libhexfive.h:23
+   function ECALL_CSRR_MCYCLE return Unsigned_64;  -- libhexfive.h:23
    pragma Import (C, ECALL_CSRR_MCYCLE, "ECALL_CSRR_MCYCLE");
 
-   function ECALL_CSRR_MINSTR return int;  -- libhexfive.h:24
+   function ECALL_CSRR_MINSTR return Unsigned_64;  -- libhexfive.h:24
    pragma Import (C, ECALL_CSRR_MINSTR, "ECALL_CSRR_MINSTR");
 
-   function ECALL_CSRR_MHPMC3 return int;  -- libhexfive.h:25
+   function ECALL_CSRR_MHPMC3 return Unsigned_64;  -- libhexfive.h:25
    pragma Import (C, ECALL_CSRR_MHPMC3, "ECALL_CSRR_MHPMC3");
 
-   function ECALL_CSRR_MHPMC4 return int;  -- libhexfive.h:26
+   function ECALL_CSRR_MHPMC4 return Unsigned_64;  -- libhexfive.h:26
    pragma Import (C, ECALL_CSRR_MHPMC4, "ECALL_CSRR_MHPMC4");
 
-   function ECALL_CSRR_MISA return int;  -- libhexfive.h:28
+   function ECALL_CSRR_MISA return Unsigned_64;  -- libhexfive.h:28
    pragma Import (C, ECALL_CSRR_MISA, "ECALL_CSRR_MISA");
 
-   function ECALL_CSRR_MVENDID return int;  -- libhexfive.h:29
+   function ECALL_CSRR_MVENDID return Unsigned_64;  -- libhexfive.h:29
    pragma Import (C, ECALL_CSRR_MVENDID, "ECALL_CSRR_MVENDID");
 
-   function ECALL_CSRR_MARCHID return int;  -- libhexfive.h:30
+   function ECALL_CSRR_MARCHID return Unsigned_64;  -- libhexfive.h:30
    pragma Import (C, ECALL_CSRR_MARCHID, "ECALL_CSRR_MARCHID");
 
-   function ECALL_CSRR_MIMPID return int;  -- libhexfive.h:31
+   function ECALL_CSRR_MIMPID return Unsigned_64;  -- libhexfive.h:31
    pragma Import (C, ECALL_CSRR_MIMPID, "ECALL_CSRR_MIMPID");
 
-   function ECALL_CSRR_MHARTID return int;  -- libhexfive.h:32
+   function ECALL_CSRR_MHARTID return Unsigned_64;  -- libhexfive.h:32
    pragma Import (C, ECALL_CSRR_MHARTID, "ECALL_CSRR_MHARTID");
 
 end MultiZone;
